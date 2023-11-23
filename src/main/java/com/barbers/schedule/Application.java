f606d2f6aea0ba7756450daa1838d0dc046ed59f
jaoguimaraes <joao.pedrogs@live.com>
2023-11-22 23:49:55 -0300

    FIX

--- src/main/java/com/barbers/schedule/Application.java
+++ src/main/java/com/barbers/schedule/Application.java
@@ -1,12 +1,7 @@
 package com.barbers.schedule;
 
-import com.barbers.schedule.domain.model.Address;
-import com.barbers.schedule.domain.model.Customer;
-import com.barbers.schedule.repository.CustomerRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
