f1bf632f87c664a2a63e025cebecde69ace48861
lucasinaciodaluz <lucasinaciodaluz@MacBook-Pro-de-Lucas.local>
2023-11-25 11:30:09 -0300

    feature: removing unnecessary code

--- src/main/java/com/barbers/schedule/Application.java
+++ src/main/java/com/barbers/schedule/Application.java
@@ -1,20 +1,9 @@
 package com.barbers.schedule;
 
-import com.barbers.schedule.domain.model.*;
-import com.barbers.schedule.repository.AddressRepository;
-import com.barbers.schedule.repository.CustomerRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.math.BigDecimal;
-import java.util.Optional;
-import java.util.Set;
 
 @Slf4j
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
