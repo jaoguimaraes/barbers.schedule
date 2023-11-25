ccdf62f7d51f3fb37ee6e84c8b7b8bde6f002e44
lucasinaciodaluz <lucasinaciodaluz@MacBook-Pro-de-Lucas.local>
2023-11-25 10:42:00 -0300

    feature: add tests

--- src/main/java/com/barbers/schedule/dto/CustomerDTO.java
+++ src/main/java/com/barbers/schedule/dto/CustomerDTO.java
@@ -1,16 +1,13 @@
 package com.barbers.schedule.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
-@Builder
 @ToString
+@Builder(access = AccessLevel.PUBLIC)
 public class CustomerDTO implements Serializable {
 
     private Long id;
---
+++ src/test/java/com/barbers/schedule/controller/CustomerControllerTest.java
@@ -0,0 +1,63 @@
+package com.barbers.schedule.controller;
+
+import com.barbers.schedule.dto.CustomerDTO;
+import com.barbers.schedule.service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerControllerTest {
+
+    private static final Long CUSTOMER_ID = 1L;
+    private static final String CUSTOMER_NAME = "Barber1";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerService customerService;
+
+    @Test
+    void itShouldGetCustomerById() throws Exception {
+        when(customerService.findCustomerById(CUSTOMER_ID)).thenReturn(buildCustomerDTO());
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(CUSTOMER_ID))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(CUSTOMER_NAME));
+    }
+
+    @Test
+    void itShouldCreateCustomer() throws Exception {
+        when(customerService.findCustomerById(CUSTOMER_ID)).thenReturn(buildCustomerDTO());
+        mockMvc.perform(MockMvcRequestBuilders.post("/customer/1", buildCustomerDTO())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(CUSTOMER_ID))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(CUSTOMER_NAME));
+    }
+
+    private CustomerDTO buildCustomerDTO() {
+        return CustomerDTO.builder()
+                .id(CUSTOMER_ID)
+                .name(CUSTOMER_NAME)
+                .build();
+    }
+
+}
