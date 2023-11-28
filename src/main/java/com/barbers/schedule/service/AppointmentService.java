4237d205d6111f7b0048ab102f14a48f00706fac
lucasinaciodaluz <lucasinaciodaluz@MacBook-Pro-de-Lucas.local>
2023-11-28 12:56:15 -0300

    feature: adding business rules to appointment

--- src/main/java/com/barbers/schedule/service/AppointmentService.java
+++ src/main/java/com/barbers/schedule/service/AppointmentService.java
@@ -67,14 +67,7 @@ public class AppointmentService {
             throw new IllegalArgumentException(String.format("Employee is not active: %d", employee.getId()));
         }
 
-        boolean isEmployeeAbleToScheduleAppointment = canEmployeeScheduleAppointment(
-                employee.getWorkStartDate(),
-                employee.getLunchStartDate(),
-                employee.getLunchEndDate(),
-                employee.getWorkEndDate(),
-                appointmentDTO.getStartDate(),
-                appointmentDTO.getEndDate()
-        );
+        boolean isEmployeeAbleToScheduleAppointment = canEmployeeScheduleAppointment(employee, appointmentDTO);
 
         if (!isEmployeeAbleToScheduleAppointment) {
             throw new IllegalArgumentException(
@@ -83,60 +76,116 @@ public class AppointmentService {
         }
     }
 
-    private boolean canEmployeeScheduleAppointment(
-            final String startDate,
-            final String lunchStartDate,
-            final String lunchEndDate,
-            final String endDate,
-            final LocalDateTime startDateAppointment,
-            final LocalDateTime endDateAppointment) {
+    private static boolean canEmployeeScheduleAppointment(final Employee employee, final AppointmentDTO appointmentDTO) {
+        List<String> startDateConverted = extractHoursAndMinutes(employee.getWorkStartDate());
+        List<String> lunchStartDateConverted = extractHoursAndMinutes(employee.getLunchStartDate());
+        List<String> lunchEndDateConverted = extractHoursAndMinutes(employee.getLunchEndDate());
+        List<String> endDateConverted = extractHoursAndMinutes(employee.getWorkEndDate());
 
-        List<String> startDateConverted = convertDate(startDate);
-        List<String> lunchStartDateConverted = convertDate(lunchStartDate);
-        List<String> lunchEndDateConverted = convertDate(lunchEndDate);
-        List<String> endDateConverted = convertDate(endDate);
+        hasUserAnotherAppointmentInThisPeriod(employee, appointmentDTO);
 
-        boolean isAfterStartDate = compareDateWithMinutes(
+        boolean isAfterStartDate = compareHoursAndMinutes(
                 Integer.parseInt(startDateConverted.get(0)),
                 Integer.parseInt(startDateConverted.get(1)),
-                startDateAppointment.getHour(),
-                startDateAppointment.getMinute()
+                appointmentDTO.getStartDate().getHour(),
+                appointmentDTO.getStartDate().getMinute()
         );
 
-        boolean isBeforeLunch = compareDateWithMinutes(
-                endDateAppointment.getHour(),
-                endDateAppointment.getMinute(),
+        boolean isBeforeLunch = compareHoursAndMinutes(
+                appointmentDTO.getEndDate().getHour(),
+                appointmentDTO.getEndDate().getMinute(),
                 Integer.parseInt(lunchStartDateConverted.get(0)),
                 Integer.parseInt(lunchStartDateConverted.get(1))
         );
 
-        boolean isAfterLunch = compareDateWithMinutes(
-                endDateAppointment.getHour(),
-                endDateAppointment.getMinute(),
+        boolean isAfterLunch = compareHoursAndMinutes(
+                appointmentDTO.getEndDate().getHour(),
+                appointmentDTO.getEndDate().getMinute(),
                 Integer.parseInt(endDateConverted.get(0)),
                 Integer.parseInt(endDateConverted.get(1))
         );
 
-        boolean isBeforeEndWork = compareDateWithMinutes(
+        boolean isBeforeEndWork = compareHoursAndMinutes(
                 Integer.parseInt(lunchEndDateConverted.get(0)),
                 Integer.parseInt(lunchEndDateConverted.get(1)),
-                startDateAppointment.getHour(),
-                startDateAppointment.getMinute()
+                appointmentDTO.getStartDate().getHour(),
+                appointmentDTO.getStartDate().getMinute()
         );
 
         return (isAfterStartDate && isBeforeLunch || isAfterLunch && isBeforeEndWork);
 
     }
 
-    private List<String> convertDate(final String date) {
+    private static void hasUserAnotherAppointmentInThisPeriod(final Employee employee, final AppointmentDTO appointmentDTO) {
+        if (!employee.getAppointments().isEmpty()) {
+            boolean hasAppointments = employee.getAppointments()
+                    .stream()
+                    .anyMatch(employeeAppointment -> isAppointmentAtSameTime(appointmentDTO, employeeAppointment)
+                            || (isAppointmentAtSameTimeOrInside(appointmentDTO, employeeAppointment)
+                            || isAppointmentBeforeBeginAndBeforeEnd(appointmentDTO, employeeAppointment))
+                            || isAppointmentAfterBeginAndAfterEnd(appointmentDTO, employeeAppointment)
+                            || isAppointmentBeforeBeginAndAfterEnd(appointmentDTO, employeeAppointment)
+                    );
+            if (hasAppointments) {
+                throw new IllegalArgumentException(
+                        String.format("Employee %d can not attend the appointment during this period: %s - %s",
+                                employee.getId(), appointmentDTO.getStartDate(), appointmentDTO.getEndDate()));
+            }
+        }
+    }
+
+    private static boolean isAppointmentAtSameTime(final AppointmentDTO appointmentDTO,
+                                                   final Appointment employeeAppointment) {
+        return appointmentDTO.getStartDate().isEqual(employeeAppointment.getStartDate())
+                && appointmentDTO.getEndDate().isEqual(employeeAppointment.getEndDate());
+    }
+
+    private static boolean isAppointmentAtSameTimeOrInside(final AppointmentDTO appointmentDTO,
+                                                           final Appointment employeeAppointment) {
+        return isAfterOrEqualDate(appointmentDTO.getStartDate(), employeeAppointment.getStartDate())
+                && isBeforeOrEqualDate(appointmentDTO.getEndDate(), employeeAppointment.getEndDate());
+    }
+
+    private static boolean isAppointmentBeforeBeginAndBeforeEnd(final AppointmentDTO appointmentDTO,
+                                                                final Appointment employeeAppointment) {
+        return (isBeforeOrEqualDate(appointmentDTO.getStartDate(), employeeAppointment.getStartDate()))
+                && isAfterOrEqualDate(appointmentDTO.getEndDate(), employeeAppointment.getStartDate())
+                && isBeforeOrEqualDate(appointmentDTO.getEndDate(), employeeAppointment.getEndDate());
+    }
+
+    private static boolean isAppointmentAfterBeginAndAfterEnd(final AppointmentDTO appointmentDTO,
+                                                              final Appointment employeeAppointment) {
+        return isAfterOrEqualDate(appointmentDTO.getStartDate(), employeeAppointment.getStartDate())
+                && isBeforeOrEqualDate(appointmentDTO.getStartDate(), employeeAppointment.getEndDate())
+                && isAfterOrEqualDate(appointmentDTO.getEndDate(), employeeAppointment.getEndDate());
+    }
+
+    private static boolean isAppointmentBeforeBeginAndAfterEnd(final AppointmentDTO appointmentDTO,
+                                                               final Appointment appointment) {
+        return isBeforeOrEqualDate(appointmentDTO.getStartDate(), appointment.getStartDate())
+                && isAfterOrEqualDate(appointmentDTO.getEndDate(), appointment.getEndDate());
+    }
+
+    private static boolean isBeforeOrEqualDate(final LocalDateTime startDate, final LocalDateTime endDate) {
+        return startDate.isBefore(endDate) || startDate.isEqual(endDate);
+
+    }
+
+    private static boolean isAfterOrEqualDate(final LocalDateTime startDate, final LocalDateTime endDate) {
+        return startDate.isAfter(endDate) || startDate.isEqual(endDate);
+
+    }
+
+    private static List<String> extractHoursAndMinutes(final String date) {
         return Arrays.stream(date.split(":"))
                 .collect(Collectors.toList());
     }
 
-    private boolean compareDateWithMinutes(final Integer hour,
-                                           final Integer minute,
-                                           final Integer appointmentHour,
-                                           final Integer appointmentMinutes) {
+    private static boolean compareHoursAndMinutes(
+            final Integer hour,
+            final Integer minute,
+            final Integer appointmentHour,
+            final Integer appointmentMinutes) {
         if (hour == appointmentHour) {
             if (minute < appointmentMinutes) {
                 return true;
